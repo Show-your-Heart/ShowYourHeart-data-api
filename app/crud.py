3ab1c2e34377fd7ce03ed146a77071006aa621fb
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-11-19 16:36:49 +0100

    export-answers

--- app/crud.py
+++ app/crud.py
@@ -227,7 +227,7 @@ def get_export_answers(db, campaign: str, method: str,  language: str = None ):
                 , id_method, method_name
                 , method_section_title, path_order
                 , id_indicator, indicator_code, indicator_name, is_direct_indicator, indicator_category, indicator_data_type
-                , case when str_value like '["%' then value else gender end as classificacio, case when str_value like '["%' then '1' else value end as valor
+                , case when str_value like '["%%' then value else gender end as classificacio, case when str_value like '["%%' then '1' else value end as valor
             from res
             order by res.vat_number, path_order, is_direct_indicator, indicator_code, classificacio   
     """

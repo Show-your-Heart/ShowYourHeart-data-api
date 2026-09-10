0abc95f8cb5f552cbbd0783a5646257a6de94f67
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2026-09-10 10:09:14 +0200

    afegir set_name

--- app/crud.py
+++ app/crud.py
@@ -270,72 +270,7 @@ def get_export_answers(db, campaign: str, method: str
                     and n.network_id='{network}'
             )   
             """ if network is not None else ""
-    # qry = f"""
-    #     with res as (
-    #         select  ac.campaign_name{lang} as campaign_name, ac."year", id_campaign
-    #             , ac.id_method,  ac.method_name{lang} as method_name
-    #             , ac.vat_number, ac.organization_name, ac.id_organization
-    #             , ac.method_section_title{lang} as method_section_title , ac.path_order
-    #             , ac.id_indicator, ac.indicator_code , ac.indicator_name{lang} as indicator_name
-    #             , ac.is_direct_indicator , ac.indicator_category , ac.indicator_data_type
-    #             {prjcols}
-    #             , unnest(translate(coalesce(ac.str_gender{lang}, ac.str_list{lang}), '[]', '{{}}')::text[]) gender
-    #             , ac.str_value{lang} as str_value
-    #             , unnest((case
-    #                 when ac.str_value not like '[%%' then '{{'||trim(replace(ac.str_value{lang},',','|'))||'}}'
-    #                 else replace(replace(translate(ac.str_value{lang}, '[]', '{{}}') , ',}}','}}'),', }}', '}}')
-    #                 end)::text[]) as value
-    #         from external.answers_calc_agg ac
-    #         where 1=1
-    #             and ac.id_campaign ='{campaign}'
-    #             and ac.id_method ='{method}'
-    #             {orga}
-    #             {prj}
-    #             {net}
-    #         order by ac.path_order , indicator_code, gender
-    #     )
-    #     , res_full as (
-    #         select distinct ac.campaign_name{lang} as campaign_name, ac."year", id_campaign
-    #             , ac.id_method,  ac.method_name{lang} as method_name
-    #             , ac.method_section_title{lang} as method_section_title , ac.path_order
-    #             , ac.id_indicator, ac.indicator_code , ac.indicator_name{lang} as indicator_name
-    #             , ac.is_direct_indicator , ac.indicator_category , ac.indicator_data_type
-    #         from external.answers_calc_agg ac
-    #         where 1=1
-    #             and ac.id_campaign ='{campaign}'
-    #             and ac.id_method ='{method}'
-    #     )
-    #      , org as (
-    #          select distinct ac.vat_number, ac.organization_name, ac.id_organization
-    #              , id_project as id_project, coalesce(project_name,'') as project_name
-    #          from external.answers_calc_agg ac
-    #          where 1=1
-    #             and ac.id_campaign ='{campaign}'
-    #             and ac.id_method ='{method}'
-    #             {orga}
-    #             {prj}
-    #     )
-    #      select f.id_campaign, f.campaign_name, f."year"
-    #          , f.id_organization, r.vat_number, f.organization_name
-    #          , f.id_project as id_project, coalesce(f.project_name,'') as project_name
-    #          , f.id_method, f.method_name
-    #          , f.method_section_title, f.path_order
-    #          , f.id_indicator, f.indicator_code, f.indicator_name, f.is_direct_indicator, f.indicator_category, f.indicator_data_type
-    #          , str_value, gender
-    #          , coalesce(case when str_value like '["%%' and gender is null then value else gender end,'') as classificacio
-    #          , case when str_value like '["%%' and gender is null then '1' else value end as valor
-    #      from (select *
-    #         from res_full f
-    #             cross join org) f
-    #         left join res r on
-    #             f.id_campaign = r.id_campaign
-    #                 and f.id_method=r.id_method
-    #                 and f.method_section_title = r.method_section_title
-    #                 and f.id_indicator = r.id_indicator
-    #                 and f.id_organization = r.id_organization
-    #                 and (f.id_project = r.id_project or f.id_project is null and r.id_project is null)
-    #      order by r.vat_number, f.path_order, f.is_direct_indicator, f.indicator_code
-    # """
+
     qry = f"""
             with res as (
                 select  ac.campaign_name{lang} as campaign_name, ac."year", id_campaign
@@ -351,7 +286,7 @@ def get_export_answers(db, campaign: str, method: str
                         when ac.str_value not like '[%%' then '{{'||trim(replace(replace(ac.str_value{lang}, '"', ''),',','|'))||'}}'  
                         else replace(replace(translate(replace(ac.str_value{lang},'"',''), '[]', '{{}}') , ',}}','}}'),', }}', '}}')
                         end)::text[]) as value
-                    , set_code, instance_number
+                    , set_code, set_name{lang} as set_name, instance_number
                 from external.answers_calc_agg_full ac 
                 where 1=1
                     and ac.id_campaign ='{campaign}'
@@ -369,13 +304,13 @@ def get_export_answers(db, campaign: str, method: str
                     , id_indicator, indicator_code, indicator_name, is_direct_indicator, indicator_category, indicator_data_type
                     , coalesce(case when str_value like '["%%' and gender is null then value else gender end,'') as classificacio
                     , case when str_value like '["%%' and gender is null then '1' else value end as valor
-                    , set_code, coalesce(instance_number, 0) as instance_number
+                    , set_code, set_name, coalesce(instance_number, 0) as instance_number
                 from res
                 order by res.vat_number, path_order, set_code, instance_number, is_direct_indicator, indicator_code, classificacio   
     """
 
     cols = ['id_campaign', 'campaign_name', '"year"', 'id_organization', 'vat_number', 'organization_name'
-            , 'set_code', 'instance_number']
+            , 'set_code', 'set_name', 'instance_number']
     cols.extend(['id_project', 'project_name'])
     # if project is not None and project != '':
     #     cols.extend(['id_project', 'project_name'])
@@ -397,7 +332,7 @@ def get_export_answers(db, campaign: str, method: str
 
     ct = pd.crosstab(
         index=[df.path_order, df.method_name, df.method_section_title
-            , df.set_code, df.instance_number
+            , df.set_code, df.set_name, df.instance_number
             , df.is_direct_indicator, df.indicator_code,
                df.indicator_name, df.classificacio]
         , columns=colsexcel, values=df.valor, aggfunc="min")
@@ -409,10 +344,11 @@ def get_export_answers(db, campaign: str, method: str
         ct.to_excel(writer, sheet_name="Resultats")
         worksheet = writer.sheets['Resultats']
         worksheet.column_dimensions['A'].hidden = True
-        worksheet.column_dimensions['F'].hidden = True
+        worksheet.column_dimensions['G'].hidden = True
         # si no hi ha sets amaguem les columnes
         worksheet.column_dimensions['D'].hidden = True if len(pd.unique(df['instance_number'])) == 1 else False
         worksheet.column_dimensions['E'].hidden = True if len(pd.unique(df['instance_number'])) == 1 else False
+        worksheet.column_dimensions['F'].hidden = True if len(pd.unique(df['instance_number'])) == 1 else False
         worksheet.column_dimensions['B'].width = 30
         worksheet.column_dimensions['C'].width = 30
         worksheet.column_dimensions['D'].width = 30
@@ -421,6 +357,7 @@ def get_export_answers(db, campaign: str, method: str
         worksheet.column_dimensions['G'].width = 30
         worksheet.column_dimensions['H'].width = 30
         worksheet.column_dimensions['I'].width = 30
+        worksheet.column_dimensions['J'].width = 30
 
         for col in range(8, 4000):
             column_letter = get_column_letter(col)
